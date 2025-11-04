48ba991a216782407821a264d870beb537f680ad
MannazX <47383214+MannazX@users.noreply.github.com>
2025-11-04 12:59:26 +0100

    Add files via upload

---
+++ Calculator-with-Selenium/js/main.js
@@ -0,0 +1,13 @@
+const app = Vue.app({
+    data() {
+        return {
+            intro : "Calculator made using Vue.js and Selenium Framework"
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+        
+    }
+});
